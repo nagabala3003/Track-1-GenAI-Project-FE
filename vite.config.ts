da829f3cfd88ca4049bd3cd09ef82af808f4a3b3
nagabala3003 <annanagabala0330@gmail.com>
2026-03-28 23:21:33 +0530

    28-03-2026-changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE');
-  const apiUrl: string = env.VITE_API_URL || 'http://localhost:8000';
+  // Load env variables
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const apiUrl = env.VITE_API_URL;
 
   return {
     plugins: [react()],
@@ -29,4 +31,3 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
-
